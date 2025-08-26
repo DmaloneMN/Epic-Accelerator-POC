9049520120f98193cd6267214e3fd257b52e13fe
DmaloneMN <87717617+DmaloneMN@users.noreply.github.com>
2025-08-26 12:18:57 -0500

    Add tenant_id and audience variables to Terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,10 @@
+
+variable "tenant_id" {
+  description = "Azure AD tenant ID"
+  type        = string
+}
+
+variable "audience" {
+  description = "FHIR service audience URL"
+  type        = string
+}
